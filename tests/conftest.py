469c329482aebb10497e88a65dc0e13f9fa27ae8
Marcelo <marcelo.augusto.39@gmail.com>
2026-04-28 15:56:51 -0300

    feat: cria campo updated_at na tabela de usuários

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,15 +30,23 @@ def session():
 @contextmanager
 def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
 
-    def fake_time_hook(mapper, connection, target):
+    def fake_time_hook_insert(mapper, connection, target):
         if hasattr(target, "created_at"):
             target.created_at = time
+        if hasattr(target, "updated_at"):
+            target.updated_at = time
 
-    event.listen(model, "before_insert", fake_time_hook)
+    def fake_time_hook_update(mapper, connection, target):
+        if hasattr(target, "updated_at"):
+            target.updated_at = time
+
+    event.listen(model, "before_insert", fake_time_hook_insert)
+    event.listen(model, "before_update", fake_time_hook_update)
 
     yield time
 
-    event.remove(model, "before_insert", fake_time_hook)
+    event.remove(model, "before_insert", fake_time_hook_insert)
+    event.remove(model, "before_update", fake_time_hook_update)
 
 
 @pytest.fixture
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,5 @@
 from dataclasses import asdict
+from datetime import datetime
 
 from sqlalchemy import select
 
@@ -21,4 +22,31 @@ def test_create_user(session, mock_db_time):
         "password": "secret",
         "email": "teste@test",
         "created_at": time,
+        "updated_at": time,
+    }
+
+
+def test_update_user(session, mock_db_time):
+    with mock_db_time(model=User, time=datetime(2024, 1, 1)) as creation_time:
+        new_user = User(
+            username="alice", password="secret", email="teste@test"
+        )
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == "alice"))
+
+    with mock_db_time(model=User, time=datetime(2024, 1, 2)) as update_time:
+        user.username = "alice.freitas"
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+    assert asdict(user) == {
+        "id": 1,
+        "username": "alice.freitas",
+        "password": "secret",
+        "email": "teste@test",
+        "created_at": creation_time,
+        "updated_at": update_time,
     }
